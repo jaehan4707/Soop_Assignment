15697b577dec92b3284afc48394659a953afcac3
jaehan4707 <jahan4707@naver.com>
2025-02-04 14:48:42 +0900

    ui(#1): Typography title, body, label 분류

--- Project/app/src/main/java/com/jaehan/soop_assignment/ui/theme/Type.kt
+++ Project/app/src/main/java/com/jaehan/soop_assignment/ui/theme/Type.kt
@@ -2,16 +2,57 @@ package com.jaehan.soop_assignment.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.jaehan.soop_assignment.R
 
+val letterSpacing = (-0.06).em
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+        fontSize = 20.sp,
+        letterSpacing = letterSpacing,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+        fontSize = 18.sp,
+        letterSpacing = letterSpacing,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_bold)),
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        letterSpacing = letterSpacing,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+        fontSize = 18.sp,
+        letterSpacing = letterSpacing,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+        fontSize = 16.sp,
+        letterSpacing = letterSpacing,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+        fontSize = 14.sp,
+        letterSpacing = letterSpacing,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+        fontSize = 16.sp,
+        letterSpacing = letterSpacing,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+        fontSize = 14.sp,
+        letterSpacing = letterSpacing,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+        fontSize = 12.sp,
+        letterSpacing = letterSpacing,
+    ),
 )
