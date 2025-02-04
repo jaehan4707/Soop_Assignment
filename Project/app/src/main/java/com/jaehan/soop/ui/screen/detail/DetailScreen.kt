8c9e1b1f583358cabdec4cb3594259526daa6b1d
jaehan4707 <jahan4707@naver.com>
2025-02-04 23:02:51 +0900

    ui(#5): 레포지토리 이름, 토픽 UI 구현

---
+++ Project/app/src/main/java/com/jaehan/soop/ui/screen/detail/DetailScreen.kt
@@ -0,0 +1,91 @@
+package com.jaehan.soop.ui.screen.detail
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jaehan.soop.ui.theme.SOOP_Theme
+import com.jaehan.soop.ui.theme.Typography
+import com.jaehan.soop.ui.theme.lightGray
+
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    repositoryName: String,
+    topics: List<String>,
+    star: Long,
+    watchers: Long,
+    fork: Long,
+    description: String,
+    userProfileImage: String,
+) {
+    Column(
+        modifier =
+        modifier
+            .fillMaxSize()
+            .background(color = lightGray)
+            .padding(vertical = 20.dp, horizontal = 10.dp),
+    ) {
+        Text(text = repositoryName, style = Typography.titleLarge)
+        LazyRow(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+            items(topics.size) { index ->
+                SuggestionChip(
+                    onClick = { },
+                    label = {
+                        Text(
+                            text = topics[index],
+                            style = Typography.bodyMedium
+                        )
+                    },
+                    border = BorderStroke(width = 1.dp, color = Color.Transparent),
+                    shape = CircleShape,
+                    colors = SuggestionChipDefaults.suggestionChipColors()
+                        .copy(containerColor = Color.LightGray),
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun DetailScreenPreview() {
+    SOOP_Theme {
+        DetailScreen(
+            repositoryName = "android",
+            topics = listOf(
+                "android",
+                "kotlin",
+                "owncloud",
+                "1234",
+                "12314125",
+                "1234",
+                "12345",
+                "12314123",
+                "123123"
+            ),
+            star = 39000,
+            watchers = 39000,
+            fork = 31000,
+            description = "asdasedadsadsad",
+            userProfileImage = "https://avatars.githubusercontent.com/u/99114456?v=4"
+        )
+    }
+}
