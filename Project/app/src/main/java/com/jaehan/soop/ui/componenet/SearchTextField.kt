7a4dacc030b9ae7b121adbaa7083cd3ac0876549
jaehan4707 <jahan4707@naver.com>
2025-02-04 16:28:02 +0900

    ui(#3): 검색어 입력 Component SearchTextField 추가

---
+++ Project/app/src/main/java/com/jaehan/soop/ui/componenet/SearchTextField.kt
@@ -0,0 +1,78 @@
+package com.jaehan.soop.ui.componenet
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jaehan.soop.R
+import com.jaehan.soop.ui.theme.SOOP_Theme
+import com.jaehan.soop.ui.theme.Typography
+import com.jaehan.soop.ui.theme.lightGray
+
+/**
+ * TODO
+ *
+ * @param modifier
+ * @param searchText : 검색어
+ * @param onTextChanged : 값이 변할 경우의 람다식
+ * @param onClickedClear : x 버튼을 누를 경우 람다식
+ * @param onClickedSearch : 키보드 완료 버튼을 누를 경우 람다식
+ * @param placeHolder : placeHolder
+ */
+@Composable
+fun SearchTextField(
+    modifier: Modifier = Modifier,
+    searchText: String = "",
+    onTextChanged: (String) -> Unit = {},
+    onClickedClear: () -> Unit = {},
+    onClickedSearch: (String) -> Unit = {},
+    placeHolder: String = "",
+) {
+    OutlinedTextField(
+        modifier =
+        modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+        value = searchText,
+        onValueChange = { onTextChanged(it) },
+        trailingIcon = {
+            IconButton(onClick = onClickedClear) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_clear),
+                    contentDescription = "clear_button",
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions =
+        KeyboardActions(onSearch = {
+            onClickedSearch(searchText)
+        }),
+        singleLine = true,
+        placeholder = {
+            Text(
+                text = placeHolder,
+                color = lightGray,
+                style = Typography.labelSmall,
+            )
+        },
+    )
+}
+
+@Composable
+@Preview(showSystemUi = true, name = "SearchTextField")
+fun SearchTextFieldPreview() {
+    SOOP_Theme {
+        SearchTextField()
+    }
+}
