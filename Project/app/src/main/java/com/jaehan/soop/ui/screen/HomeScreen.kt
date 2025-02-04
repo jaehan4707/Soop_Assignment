5185b6b2503aa40cae6d478bb564df0f5adf1285
jaehan4707 <jahan4707@naver.com>
2025-02-04 16:38:54 +0900

    ui(#3): HomeScreen 검색 입력 필드 연결

---
+++ Project/app/src/main/java/com/jaehan/soop/ui/screen/HomeScreen.kt
@@ -0,0 +1,51 @@
+package com.jaehan.soop.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jaehan.soop.R
+import com.jaehan.soop.ui.componenet.SearchTextField
+import com.jaehan.soop.ui.theme.SOOP_Theme
+import com.jaehan.soop.ui.theme.lightGray
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val (searchText, setSearchText) =
+        remember {
+            mutableStateOf("")
+        }
+    Column(
+        modifier =
+        modifier
+            .fillMaxSize()
+            .background(color = lightGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(15.dp))
+        SearchTextField(
+            modifier = Modifier,
+            searchText = searchText,
+            onTextChanged = { setSearchText(it) },
+            onClickedClear = { setSearchText("") },
+            placeHolder = stringResource(id = R.string.search_placeholder),
+        )
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun MainScreenPreview() {
+    SOOP_Theme {
+        MainScreen()
+    }
+}
