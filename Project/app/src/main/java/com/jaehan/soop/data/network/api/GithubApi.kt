b22abf4893c9d89986828892735f689b094276dc
jaehan4707 <jahan4707@naver.com>
2025-02-06 02:25:11 +0900

    feat(#17):  레포지토리 정보 조회 API 구현

--- Project/app/src/main/java/com/jaehan/soop/data/network/api/GithubApi.kt
+++ Project/app/src/main/java/com/jaehan/soop/data/network/api/GithubApi.kt
@@ -1,8 +1,10 @@
 package com.jaehan.soop.data.network.api
 
+import com.jaehan.soop.data.network.response.GetRepositoryInfoResponse
 import com.jaehan.soop.data.network.response.SearchRepositoriesResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubApi {
@@ -12,4 +14,10 @@ interface GithubApi {
         @Query("page") page: Int,
         @Query("per_page") perPage: Int = 30,
     ): Response<SearchRepositoriesResponse>
+
+    @GET("repos/{owner}/{repo}")
+    suspend fun getRepositoryInfo(
+        @Path("owner") owner: String,
+        @Path("repo") repositoryName: String,
+    ): Response<GetRepositoryInfoResponse>
 }
--- Project/app/src/main/java/com/jaehan/soop/data/repositoryimpl/RepoRepositoryImpl.kt
+++ Project/app/src/main/java/com/jaehan/soop/data/repositoryimpl/RepoRepositoryImpl.kt
@@ -4,10 +4,15 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.jaehan.soop.data.network.api.GithubApi
+import com.jaehan.soop.data.network.response.GetRepositoryInfoResponse
 import com.jaehan.soop.data.network.response.RepoPagingSource
+import com.jaehan.soop.data.network.response.mapToRepo
+import com.jaehan.soop.data.network.safeApiCall
+import com.jaehan.soop.domain.model.ApiResponse
 import com.jaehan.soop.domain.model.Repo
 import com.jaehan.soop.domain.repository.RepoRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class RepoRepositoryImpl @Inject constructor(
@@ -21,5 +26,21 @@ class RepoRepositoryImpl @Inject constructor(
             ),
             pagingSourceFactory = { RepoPagingSource(api, query) }
         ).flow
+    }
+
+    override suspend fun getRepositoryInfo(
+        owner: String,
+        repositoryName: String
+    ): Flow<ApiResponse<Repo>> = flow {
+        val response = safeApiCall(
+            apiCall = {
+                api.getRepositoryInfo(owner = owner, repositoryName = repositoryName)
+            },
+            default = GetRepositoryInfoResponse()
+        )
+        when (response) {
+            is ApiResponse.Error -> emit(response)
+            is ApiResponse.Success -> emit(ApiResponse.Success(data = response.data.mapToRepo()))
+        }
     }
 }
